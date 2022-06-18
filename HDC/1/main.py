3c1dff754d60158625731b4db146122fcee7b99f
Vitalii Nasikovskyi <ncxr.official@gmail.com>
2022-06-18 11:50:44 +0300

    Added day_1 project

---
+++ HDC/1/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+band_name = input("What`s name of the city you grew up in?\n")
+pet_name = input("What`s your pet`s name ?\n")
+
+print(f"Your band name could be {band_name} {pet_name}")
+
