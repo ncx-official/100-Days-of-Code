d4c0f9d3957b3d483f8e68ae8059a995102bce17
Vitalii <ncxr.official@gmail.com>
2022-06-26 18:01:54 +0300

    Deleted 'both' type of symbols and changed user input

--- HDC/5/passgen.py
+++ HDC/5/passgen.py
@@ -2,6 +2,9 @@ from os import system, name as systemName
 import random
 import string
 import argparse
+import re
+
+import asciiArts
 
 def ClearScreen():
     if systemName == 'nt':
@@ -10,17 +13,7 @@ def ClearScreen():
         _ = system('clear')
 
 def AboutProgram():
-    logo = '''
-     _____                               
-    |  __ \                              
-    | |__) |_ _ ___ ___  __ _  ___ _ __  
-    |  ___/ _` / __/ __|/ _` |/ _ \ '_ \ 
-    | |  | (_| \__ \__ \ (_| |  __/ | | |
-    |_|   \__,_|___/___/\__, |\___|_| |_|
-                        __/ |           
-                        |___/             by Ncx (ver. 2.0)
-    '''
-    print(logo)
+    print(asciiArts.passgen_logo)
     
 def AskToQuit():
     userInput = input("Input 'q' to exit or press enter to continue\n--> ")
@@ -35,37 +28,39 @@ def GetSymbolsCount(question):
             raise ValueError
     except ValueError:
         input("\nBad value, please try again...\n")
-        exit()  
+        return  
     return symbCount
 
-def GetSymbolsOption(question):
+def GetSymbolsOption():
     options = {
         "uppercase": False,
         "lowercase": False,
-        "both": False,
         "numbers": False,
         "special": False
     }
 
-    print(question)
-    isLetterCase = input(" - UPPERCASE (input 'u')\n - lowercase (input 'l')\n - BoTh (input 'b')\n--> ").lower()
-    if isLetterCase == 'u':
-        options["uppercase"] = True    
-    elif isLetterCase == 'l':
-        options["lowercase"] = True
-    elif isLetterCase == 'b':
-        options["both"] = True
-    ClearScreen()
+    print(asciiArts.dice)
+    print("\nChoose your password options: (To use all the options type 'ulns')")
+    print(" - UPPERCASE (input 'u')")
+    print(" - lowercase (input 'l')")
+    print(" - numbers 0123..89 (input 'n')")
+    print(" - special symbols ~!@#$%^..?/ (input 's')")
+    optionsInput = input("--> ").lower()
     
-    print(question)
-    isNumbers = input(" - numbers 0123..89 (input 'n')\n--> ").lower()
-    if isNumbers == 'n':
-        options["numbers"] = True
-    ClearScreen()
+    if optionsInput == '' or re.search('[^ulns]', optionsInput):
+        input("Wrong input! Try again...\n")
+        return
 
-    print(question)
-    isSpecialChar = input(" - special symbols ~!@#$%^..?/ (input 's')\n--> ").lower()
-    if isSpecialChar == 's':
+    if 'u' in optionsInput:
+        options["uppercase"] = True    
+    
+    if 'l' in optionsInput:
+        options["lowercase"] = True
+    
+    if 'n' in optionsInput:
+        options["numbers"] = True
+
+    if 's' in optionsInput:
         options["special"] = True
 
     return options
@@ -76,8 +71,6 @@ def CreateSymbolsString(options):
         symbolsString += string.ascii_uppercase
     if options["lowercase"]:
         symbolsString += string.ascii_lowercase
-    if options["both"]:
-        symbolsString += string.ascii_letters
     if options["numbers"]:
         symbolsString += string.digits
     if options["special"]:
@@ -99,7 +92,6 @@ def StartUpArgsParse():
     argParser.add_argument("-pl", "--password_length", help="Input count of password characters", type=int)
     argParser.add_argument("-u", "--uppercase", help="To generate password with uppercase characters", action="store_true")
     argParser.add_argument("-l", "--lowercase", help="To generate password with lowercase characters", action="store_true")
-    argParser.add_argument("-b", "--bothcase", help="To generate password with uppercase and lowercase characters", action="store_true")
     argParser.add_argument("-n", "--numbers", help="To generate password with numbers", action="store_true")
     argParser.add_argument("-s", "--special_symbols", help="To generate password with special symbols", action="store_true")
     args = argParser.parse_args()
@@ -110,7 +102,6 @@ def StartUpArgsParse():
     options = {
         "uppercase": args.uppercase,
         "lowercase": args.lowercase,
-        "both": args.bothcase,
         "numbers": args.numbers,
         "special": args.special_symbols
     }
@@ -124,13 +115,15 @@ def MenuScreen():
         AboutProgram()
 
         symbCount = GetSymbolsCount("Enter number of password length: ")
+        if symbCount == None: continue
         ClearScreen()
         
-        symbString = CreateSymbolsString(GetSymbolsOption("Choose your password options: (Leave blank ('') to skip option)"))
+        options = GetSymbolsOption()
+        if options == None: continue
+        symbString = CreateSymbolsString(options)
         ClearScreen()
 
         password = GeneratePassword(symbString, symbCount)
-
         print(f"\nYour new password: {password}\n" + "-------------------------------\n")
         AskToQuit()
 
